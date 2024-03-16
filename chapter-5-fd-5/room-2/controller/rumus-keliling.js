b52c209f6bdfc82a16715b801fc37dae968e3232
DonDfaiz <ah.faiz9297@gmail.com>
2024-03-16 22:00:38 +0700

    test

--- chapter-5-fd-5/room-2/controller/rumus-keliling.js
+++ chapter-5-fd-5/room-2/controller/rumus-keliling.js
@@ -1,17 +1,20 @@
-const history = require("../model/history")
+const history = require("../model/history");
 
-function hitungKeliling(req, res){
-    const data = req.body
-    const alas = data.alas
-    const tinggi = data.tinggi
-    const keliling = alas+ 2 * tinggi
-    res.status(200).send('Keliling:' + keliling)
+function hitungKeliling(req, res) {
+  const data = req.body;
+  const sisiA = data.sisiA;
+  const sisiB = data.sisiB;
+  const sisiC = data.sisiC;
+  //! error rumus
+  const keliling = sisiA + sisiB + sisiC;
+  res.status(200).send("Keliling:" + keliling);
 
-    history.push({
-        Alas : alas,
-        Tinggi : tinggi,
-        Keliling: keliling
-    })
-};
+  history.push({
+    sisiA: sisiA,
+    sisiB: sisiB,
+    sisiC: sisiC,
+    Keliling: keliling,
+  });
+}
 
 module.exports = hitungKeliling;
